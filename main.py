6b406474e0a9084b204182f0b4fe7179c7736881
piotrek <piotrek@ubuntu.com>
2025-02-22 18:32:42 +0100

    add checking cells and switching players

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+import random
+
 
 def print_tictactoe():
-    print(table[0])
-    print(table[1])
-    print(table[2])
+    for level in table:
+        print(level)
 
 table = [['', '', ''], ['', '', ''], ['', '', '']]
+marks=['X','O']
+player1=marks[0]
+player2=marks[1]
+players=[player1,player2]
 
 print_tictactoe()
-col=int(input('Podaj kolumne do ktorej chcesz wpisac'))
-row=int(input('podaj wiersz do ktorego chcesz wpisac'))
+is_game_on=True
+current_player=random.choice(players)
+while is_game_on:
 
 
-# sprawdzenie czy komorka jest zajeta
-if table[col][row]=='':
 
-else:
-    print('Pole jest zajete, wybierz inne')
+    print(f'Obency gracz to: {current_player}')
+    col=int(input('Podaj kolumne do ktorej chcesz wpisac'))
+    row=int(input('podaj wiersz do ktorego chcesz wpisac'))
+
+
+    # sprawdzenie czy komorka jest zajeta
+    try:
+        cell=table[row-1][col-1]
+    except IndexError:
+        print('Nie ma takiej komorki wybierz inna')
+    else:
+        if table[row-1][col-1]=='':
+            table[row-1][col-1]=current_player
+            if current_player==player1:
+                current_player=player2
+            else:
+                current_player=player1
+            print('Zmiana gracza')
+        else:
+            print('Pole jest zajete, wybierz inne')
+
+
+    print_tictactoe()
